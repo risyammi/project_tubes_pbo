b4bcbfbe1d47db6aaa52ffe9557120ec1de06a7b
Tabhitacantik <keziatabhitasmith@gmail.com>
2025-12-07 21:05:17 +0700

    up

--- Warteg/src/main/java/config/ConnectionDB.java
+++ Warteg/src/main/java/config/ConnectionDB.java
@@ -1,3 +1,10 @@
+/* Nama Kelompok                  :
+ * Kezia Tabhita Smith              140810240020
+ * Risyam Muhammad Iesqillah		140810240032
+ * Laudzan Ananda Syawaliandi		140810240050
+ * Dikumpulkan tanggal            : 7 Desember 2025
+*/
+
 package config;
 
 import java.sql.Connection;
